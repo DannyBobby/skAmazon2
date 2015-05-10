123708a13813766cc1cf2747c05408940a14fc5d
blantonjm3 <blantonjm3@vcu.edu>
2015-05-10 14:21:32 -0400

    beep boop is this how github works
    added a (probably useless) Checkout controller and viewfolder.
    also changed the item displays from Browse and Home to have  a working
    "Add to Cart" button in place of the dead "buy now" buttons.

---
+++ skAmazon2/Models/CheckoutModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace skAmazon2.Models
+{
+    public class Checkout
+    {
+        public string shipaddress { get; set; }
+
+        public string billaddress { get; set; }
+
+        public string ccnumber { get; set; }
+
+        public string ccname { get; set; }
+
+        public int ccexpirationdate { get; set; }
+    }
+}
